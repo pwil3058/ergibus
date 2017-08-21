d9e6bc69a43dcfa749af9ca32c2160e92db24cf5
Peter Williams <pwil3058@gmail.com>
2017-08-21 16:41:41 +1000

    snapshot: serialize a snapshot into a JSON string
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,59 @@
+#[macro_use]
+extern crate serde_derive;
+
+extern crate serde;
+extern crate serde_json;
+
 use std::io;
-use std::ffi::OsString;
 use std::fs::{self, Metadata};
 use std::path::{Path, PathBuf};
 use std::collections::HashMap;
+use std::os::linux::fs::MetadataExt;
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Attributes {
+    st_mode: u32,
+}
+
+impl Attributes {
+    pub fn new(metadata: &Metadata) -> Attributes {
+        Attributes{
+            st_mode: metadata.st_mode(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 struct SnapshotFile {
     path: PathBuf,
-    attributes: Metadata,
+    attributes: Attributes,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 struct SnapshotSymLink {
     path: PathBuf,
-    attributes: Metadata,
+    attributes: Attributes,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 struct SnapshotDir {
     path: PathBuf,
-    attributes: Metadata,
-    subdirs: HashMap<OsString, SnapshotDir>,
-    files: HashMap<OsString, SnapshotFile>,
-    file_links: HashMap<OsString, SnapshotSymLink>,
-    subdir_links: HashMap<OsString, SnapshotSymLink>,
+    attributes: Attributes,
+    subdirs: HashMap<String, SnapshotDir>,
+    files: HashMap<String, SnapshotFile>,
+    file_links: HashMap<String, SnapshotSymLink>,
+    subdir_links: HashMap<String, SnapshotSymLink>,
 }
 
 impl SnapshotDir {
     pub fn new(rootdir: &Path) -> io::Result<SnapshotDir> {
-        let attributes = rootdir.metadata()?;
+        let metadata = rootdir.metadata()?;
         let path = rootdir.canonicalize()?;
 
-        let mut subdirs = HashMap::<OsString, SnapshotDir>::new();
-        let mut files = HashMap::<OsString, SnapshotFile>::new();
-        let mut file_links = HashMap::<OsString, SnapshotSymLink>::new();
-        let mut subdir_links = HashMap::<OsString, SnapshotSymLink>::new();
+        let mut subdirs = HashMap::<String, SnapshotDir>::new();
+        let mut files = HashMap::<String, SnapshotFile>::new();
+        let mut file_links = HashMap::<String, SnapshotSymLink>::new();
+        let mut subdir_links = HashMap::<String, SnapshotSymLink>::new();
 
         for entry in fs::read_dir(path.as_path())? {
             let entry = entry?;
@@ -39,34 +61,34 @@ impl SnapshotDir {
             let epath = epathbuf.as_path();
             match epath.file_name() {
                 Some(file_name) => {
-                    let eattributes = epath.metadata()?;
-                    if eattributes.is_dir() {
-                        if eattributes.file_type().is_symlink() {
+                    let emetadata = epath.metadata()?;
+                    if emetadata.is_dir() {
+                        if emetadata.file_type().is_symlink() {
                             let snapshot_sym_link = SnapshotSymLink{
                                 path: epath.to_path_buf(),
-                                attributes: eattributes
+                                attributes: Attributes::new(&emetadata)
                             };
-                            subdir_links.insert(file_name.to_os_string(), snapshot_sym_link);
+                            subdir_links.insert(file_name.to_str().unwrap().to_string(), snapshot_sym_link);
                         } else {
                             let snapshot_dir = match SnapshotDir::new(epath) {
                                 Ok(ssd) => ssd,
                                 Err(_) => continue,
                             };
-                            subdirs.insert(file_name.to_os_string(), snapshot_dir);
+                            subdirs.insert(file_name.to_str().unwrap().to_string(), snapshot_dir);
                         }
                     } else {
-                        if eattributes.file_type().is_symlink() {
+                        if emetadata.file_type().is_symlink() {
                             let snapshot_sym_link = SnapshotSymLink{
                                 path: epath.to_path_buf(),
-                                attributes: eattributes
+                                attributes: Attributes::new(&emetadata)
                             };
-                            file_links.insert(file_name.to_os_string(), snapshot_sym_link);
+                            file_links.insert(file_name.to_str().unwrap().to_string(), snapshot_sym_link);
                         } else {
                             let snapshot_file = SnapshotFile{
                                 path: epath.to_path_buf(),
-                                attributes: eattributes
+                                attributes: Attributes::new(&emetadata)
                             };
-                            files.insert(file_name.to_os_string(), snapshot_file);
+                            files.insert(file_name.to_str().unwrap().to_string(), snapshot_file);
                         }
                     }
                 }
@@ -75,7 +97,7 @@ impl SnapshotDir {
         }
         Ok(SnapshotDir {
             path: path,
-            attributes: attributes,
+            attributes: Attributes::new(&metadata),
             subdirs: subdirs,
             files: files,
             file_links: file_links,
@@ -88,9 +110,11 @@ fn main() {
     let p = Path::new(".");
     let sd = match SnapshotDir::new(p) {
         Ok(ssd) => ssd,
-        Err(_) => panic!("bummer")
+        Err(e) => panic!("bummer: {:?}", e)
     };
-    let pc = p.canonicalize();
-    println!("path = {:?} ({:?}) {:?} {:?}", p.to_str(), pc, sd.path, sd.attributes);
-    println!("Hello, world!");
+    let sd_str = match serde_json::to_string(&sd) {
+        Ok(ok) => ok,
+        Err(e) => panic!("double bummer: {:?}", e)
+    };
+    println!("JSON string:\n{:?}", sd_str)
 }
