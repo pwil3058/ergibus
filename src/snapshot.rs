04d6dd836f56f64ed656c0b8d7c00623cfcc362c
Peter Williams <pwil3058@gmail.com>
2017-09-10 11:46:56 +1000

    copyright: put copyright statements at the beginning of source files
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- src/snapshot.rs
+++ src/snapshot.rs
@@ -1,3 +1,17 @@
+// Copyright 2017 Peter Williams <pwil3058@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Standard Library access
 use std::collections::HashMap;
 use std::fs::{self, Metadata, File};
@@ -14,7 +28,7 @@ use serde_json;
 use snap;
 use walkdir::{WalkDir, WalkDirIterator};
 
-// local crates access
+// local modules access
 use archive::{AError, Exclusions, ArchiveData, get_archive_data};
 use content::{ContentMgmtKey, ContentManager, CError, get_content_mgmt_key};
 use pathux::{first_subpath_as_string};
