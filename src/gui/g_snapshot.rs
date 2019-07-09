24300947292b2034f5e9859c6c8ef1f257a57345
Peter Williams <pwil3058@gmail.com>
2019-07-09 16:06:37 +1000

    License: change to dual MIT/Apache license
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- src/gui/g_snapshot.rs
+++ src/gui/g_snapshot.rs
@@ -1,17 +1,3 @@
-// Copyright 2017 Peter Williams <pwil3058@gmail.com>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 use std::cell::RefCell;
 use std::io::Write;
 use std::rc::Rc;
@@ -56,7 +42,7 @@ impl RowBuffer<Vec<String>> for SnapshotRowBuffer {
         match self.archive_name {
             Some(ref archive_name) => {
                 match snapshot::get_snapshot_names_for_archive(archive_name, true) {
-                    Ok(mut snapshot_names) => {
+                    Ok(snapshot_names) => {
                         let hash = generate_digest(&snapshot_names);
                         core.set_raw_data(snapshot_names, hash);
                     },
