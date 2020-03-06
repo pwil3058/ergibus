74cb2081b1e3b4bf436d27d9931061716e737f6f
Peter Williams <pwil3058@gmail.com>
2020-03-06 10:12:45 +1000

    ergibus_lib: use log to report errors etc.
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- ergibus_lib/src/report.rs
+++ ergibus_lib/src/report.rs
@@ -2,31 +2,44 @@ use std::error::Error;
 use std::io::{self, ErrorKind};
 use std::path::Path;
 
+use log;
+
 pub fn ignore_report_or_crash(err: &io::Error, path: &Path) {
-    if err.kind() != ErrorKind::NotFound {
-        // we assume that "not found" is due to a race condition and don't report it
-        if err.kind() == ErrorKind::PermissionDenied {
-            // benign so just report it
-            println!("{:?}: permission denied", path);
-        } else {
+    match err.kind() {
+        // we assume that "not found" is due to a race condition
+        ErrorKind::NotFound => log::trace!("{:?}: not found", path),
+        // benign so just report it
+        ErrorKind::PermissionDenied => log::warn!("{:?}: permission denied", path),
+        // programming error that needs to be fixed
+        _ => {
+            log::error!("{:?}: {:?}: {:?}", err.kind(), err.description(), path);
             panic!("{:?}: {:?}: {:?}", err.kind(), err.description(), path);
         }
     }
 }
 
 pub fn report_broken_link_or_crash(err: &io::Error, link_path: &Path, target_path: &Path) {
-    if err.kind() == ErrorKind::NotFound {
-        println!(
+    match err.kind() {
+        ErrorKind::NotFound => log::warn!(
             "{:?} -> {:?}: broken symbolic link ignored",
-            link_path, target_path
-        );
-    } else {
-        panic!(
-            "{:?}: {:?}: {:?} -> {:?}",
-            err.kind(),
             link_path,
-            target_path,
-            err.description()
-        );
+            target_path
+        ),
+        _ => {
+            log::error!(
+                "{:?}: {:?}: {:?} -> {:?}",
+                err.kind(),
+                link_path,
+                target_path,
+                err.description()
+            );
+            panic!(
+                "{:?}: {:?}: {:?} -> {:?}",
+                err.kind(),
+                link_path,
+                target_path,
+                err.description()
+            );
+        }
     }
 }
