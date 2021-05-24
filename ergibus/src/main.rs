c2373030e3ca93f561904d453acc7986c1c2948f
Peter Williams <pwil3058@bigpond.net.au>
2021-05-25 09:46:06 +1000

    ergibus: turn on logging using stderrlog crate
    Signed-off-by: Peter Williams <pwil3058@bigpond.net.au>

--- ergibus/src/main.rs
+++ ergibus/src/main.rs
@@ -4,15 +4,35 @@ mod archive_sub_cmds;
 mod repository_sub_cmds;
 mod snapshot_sub_cmds;
 
+use log::*;
+use stderrlog;
 use structopt::StructOpt;
 
 use crate::archive_sub_cmds::ManageArchives;
 use crate::repository_sub_cmds::ManageRepositories;
 use crate::snapshot_sub_cmds::{BackUp, SnapshotContents, SnapshotManager};
 
+/// A StructOpt example
+#[derive(StructOpt, Debug)]
+#[structopt()]
+struct Ergibus {
+    /// Silence all output
+    #[structopt(short = "q", long = "quiet")]
+    quiet: bool,
+    /// Verbose mode (-v, -vv, -vvv, etc)
+    #[structopt(short = "v", long = "verbose", parse(from_occurrences))]
+    verbose: usize,
+    /// Timestamp (sec, ms, ns, none)
+    #[structopt(short = "t", long = "timestamp")]
+    ts: Option<stderrlog::Timestamp>,
+    /// Sub commands
+    #[structopt(subcommand)]
+    sub_cmd: SubCommands,
+}
+
 #[derive(Debug, StructOpt)]
 /// Experimental Rust Git Inspired Back Up System
-enum Ergibus {
+enum SubCommands {
     /// Manage archives
     #[structopt(alias = "ar")]
     Archive(ManageArchives),
@@ -33,14 +53,22 @@ enum Ergibus {
 fn main() {
     let ergibus = Ergibus::from_args();
 
-    if let Err(err) = match ergibus {
-        Ergibus::Archive(sub_cmd) => sub_cmd.exec(),
-        Ergibus::Repo(sub_cmd) => sub_cmd.exec(),
-        Ergibus::ManageSnapshots(sub_cmd) => sub_cmd.exec(),
-        Ergibus::SnapshotContents(sub_cmd) => sub_cmd.exec(),
-        Ergibus::BackUp(sub_cmd) => sub_cmd.exec(),
+    stderrlog::new()
+        //.module(module_path!())
+        .quiet(ergibus.quiet)
+        .verbosity(ergibus.verbose)
+        .timestamp(ergibus.ts.unwrap_or(stderrlog::Timestamp::Off))
+        .init()
+        .unwrap();
+
+    if let Err(err) = match ergibus.sub_cmd {
+        SubCommands::Archive(sub_cmd) => sub_cmd.exec(),
+        SubCommands::Repo(sub_cmd) => sub_cmd.exec(),
+        SubCommands::ManageSnapshots(sub_cmd) => sub_cmd.exec(),
+        SubCommands::SnapshotContents(sub_cmd) => sub_cmd.exec(),
+        SubCommands::BackUp(sub_cmd) => sub_cmd.exec(),
     } {
-        println!("failed: {:?}", err);
+        error!("{:?}", err);
         std::process::exit(1);
     }
 }
