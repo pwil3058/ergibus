7c85b5c356ca7a1598936ef83f9181b666359daa
Peter Williams <pwil3058@bigpond.net.au>
2021-07-10 13:24:28 +1000

    ergibus_orbtk: silence warning errors
    Signed-off-by: Peter Williams <pwil3058@bigpond.net.au>

--- ergibus_orbtk/src/orbtk_snapshots.rs
+++ ergibus_orbtk/src/orbtk_snapshots.rs
@@ -1,6 +1,6 @@
 // Copyright 2021 Peter Williams <pwil3058@gmail.com> <pwil3058@bigpond.net.au>
 
-use crate::orbtk_archive::{ArchiveSelectionState, ArchiveSelectionView};
+use crate::orbtk_archive::ArchiveSelectionView;
 //use ergibus_lib::snapshot;
 use orbtk::prelude::*;
 
