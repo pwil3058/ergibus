4be8ca7aa54ee54ec041e0d42a525d03bb82ace7
Peter Williams <pwil3058@bigpond.net.au>
2024-05-23 13:09:23 +1000

    Bring over tree_store from pw_gix

--- pw_gtk_ext/src/gtkx.rs
+++ pw_gtk_ext/src/gtkx.rs
@@ -1,8 +1,5 @@
 // Copyright 2021 Peter Williams <pwil3058@gmail.com> <pwil3058@bigpond.net.au>
 
-pub mod list_store;
-pub mod tree_model;
-pub mod value;
 //
 pub mod buffered_list_store;
 pub mod check_button;
@@ -12,9 +9,13 @@ pub mod dialog_user;
 pub mod drawing_area;
 pub mod entry;
 pub mod list;
+pub mod list_store;
 pub mod menu;
 pub mod notebook;
 pub mod paned;
 pub mod radio_button;
+pub mod tree_model;
+pub mod tree_store;
 pub mod tree_view;
+pub mod value;
 pub mod window;
